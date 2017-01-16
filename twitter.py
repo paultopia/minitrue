94c18cf8a353b027813744a4617b3b038dfa14bb
Paul Gowder <paul.gowder@gmail.com>
2017-01-15 19:43:59 -0600

    docstring for twitter module and have it return dict not string

--- twitter.py
+++ twitter.py
@@ -1,7 +1,7 @@
-import tweepy, json
+"""
+Hyper-simplified functionality to post to twitter (using tweepy library).
 
-def twitter_poster(json_creds):
-    """Pass me a json file with twitter creds.
+Requires json file with twitter creds.
     Fields:
     'consumer_key',
     'consumer_secret
@@ -10,15 +10,29 @@ def twitter_poster(json_creds):
     which are all in the order given in the twitter dev page
     (see tutorial: https://www.digitalocean.com/community/tutorials/how-to-create-a-twitter-app)
 
-    Returns:
-    a function to call to post to the given twitter account.
-
-    the function just posts a string and returns a json string of the form:
-    {"date": POST DATE STRING IN ISO FORMAT,
+twitter_poster is a factory-type function that spits out a function to post to twitter given by creds and return a dict string with
+    Fields:
+    "date": POST DATE STRING IN ISO FORMAT,
     "url": URL TO TWEET,
     "account": TWITTER SCREENNAME,
     "id": TWITTER ID (INT),
-    "text": TEXT OF TWEET}
+    "text": TEXT OF TWEET
+
+example:
+make_post = twitter_poster("my twitter_creds.json")
+post = make_post("testing, 1 2 3")
+print(post)
+
+"""
+
+import tweepy, json
+
+def twitter_poster(json_creds):
+    """Pass me a json file with twitter creds.
+
+    Returns:
+    a function to call to post to the given twitter account and get dict with relevant info
+
     """
     with open(json_creds) as tj:
         tcreds = json.loads(tj.read())
@@ -28,10 +42,6 @@ def twitter_poster(json_creds):
     def post_tweet(text):
         sobj = twitter.update_status(text)
         url = "https://twitter.com/" + sobj.user.screen_name + "/status/" + str(sobj.id)
-        return json.dumps({"text": sobj.text, "id": sobj.id, "date": sobj.created_at.isoformat(), "account": sobj.user.screen_name, "url": url})
+        return {"text": sobj.text, "id": sobj.id, "date": sobj.created_at.isoformat(), "account": sobj.user.screen_name, "url": url}
     return post_tweet
 
-# example:
-# make_post = twitter_poster("my twitter_creds.json")
-# post = make_post("testing, 1 2 3")
-# print(post)
